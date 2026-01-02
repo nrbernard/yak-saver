bfc845c50a679ed0de9b57cc6eda8bde6751ce7b
Nick Bernard <nick.bernard@hey.com>
2026-01-01 20:42:54 -0800

    feat: projects endpoint with placeholder data

--- cmd/main.go
+++ cmd/main.go
@@ -4,13 +4,18 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/nrbernard/yak-saver/data"
 )
 
 func main() {
 	e := echo.New()
 
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+	e.GET("/projects", func(c echo.Context) error {
+		projects := data.GetProjects()
+		response := map[string]interface{}{
+			"projects": projects,
+		}
+		return c.JSON(http.StatusOK, response)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
