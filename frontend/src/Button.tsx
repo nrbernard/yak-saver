b4613a44e6acd9c668a743f079ac391ad56df080
Nick Bernard <nick.bernard@hey.com>
2026-01-11 21:09:57 -0800

    fix: dark theme styles

--- frontend/src/Button.tsx
+++ frontend/src/Button.tsx
@@ -1,10 +1,12 @@
 import type { ButtonHTMLAttributes } from "react";
 
-export function Button(props: ButtonHTMLAttributes<HTMLButtonElement>) {
-  const { children, className, ...rest } = props;
+export function Button(
+  props: Omit<ButtonHTMLAttributes<HTMLButtonElement>, "className">
+) {
+  const { children, ...rest } = props;
   return (
     <button
-      className={`px-4 py-1 text-sm hover:cursor-pointer rounded-md bg-banana hover:text-white ${className}`}
+      className={`px-4 py-1 text-sm hover:cursor-pointer rounded-md bg-banana hover:text-white dark:bg-strawberry dark:text-dark-text`}
       {...rest}
     >
       {children}
